4798e3b731cb48c73e6a0b49e135654eaac36bd1
Roy Chen <roy75117.mobile@gmail.com>
2021-11-29 15:44:40 +0900

    Create free_space.sh
    Free up disk space

---
+++ free_space.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+# CLEAN linux-image pkg
+dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+apt-get autoremove
+apt-get update
+
+# CLEAN journal logs (default 3days)
+journalctl --vacuum-time=3d
